a213f500eb8e9b7bc55da0f95097dabf50d1ddae
Constantine Linnick <199627+theaspect@users.noreply.github.com>
2013-11-04 23:19:31 +0700

    Add test case to npo and change log levels

--- src/main/java/com/blazer/npo/NPOApp.java
+++ src/main/java/com/blazer/npo/NPOApp.java
@@ -5,12 +5,13 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.stat.Statistics;
 
 import javax.inject.Provider;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import java.util.Random;
 import java.util.UUID;
 
 /** @author Constantine Linnick <theaspect@gmail.com> */
@@ -26,6 +27,7 @@ public class NPOApp {
 
         app.init();
         app.lazyLoad();
+        app.naiveFetch();
         app.cacheWarmUpFetch();
         app.cacheWarmUp();
         app.cacheWarmUpNested();
@@ -41,16 +43,42 @@ public class NPOApp {
     }
 
     private void lazyLoad() {
-        log.debug("*** Show lazy loading");
+        log.debug("*** Lazy loading");
         Dao dao = daoProvider.get();
         long count = 0;
         try {
-            count = dao.getStatistics().getPrepareStatementCount();
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount();
             log.debug("Iterate");
             for (Parent p : dao.parentsFindAll()) {
                 log.trace(p.toString());
             }
-            count = dao.getStatistics().getPrepareStatementCount() - count;
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count;
+        } finally {
+            NPOApp.log.debug("Total queries {}", count);
+            dao.getEntityManager().close();
+        }
+    }
+
+    private void naiveFetch() {
+        log.debug("*** Naive fetch");
+        Dao dao = daoProvider.get();
+        long count = 0;
+        try {
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount();
+            dao.childMtMFindAll();
+            log.debug("Get MtM {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
+            dao.childOtMFindAll();
+            log.debug("Get OtM {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
+            dao.childOtOFindAll();
+            log.debug("Get OtO {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
+            dao.childOtMNestedFindAll();
+            log.debug("Get OtMNested {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
+
+            log.debug("Iterate");
+            for (Parent p : dao.parentsFindAll()) {
+                log.trace(p.toString());
+            }
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count;
         } finally {
             NPOApp.log.debug("Total queries {}", count);
             dao.getEntityManager().close();
@@ -62,20 +90,20 @@ public class NPOApp {
         Dao dao = daoProvider.get();
         long count = 0;
         try {
-            count = dao.getStatistics().getPrepareStatementCount();
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount();
             dao.parentsJoinMtM();
-            log.debug("Get P + MtM {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get P + MtM {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.childMtMJoinOtMNested();
-            log.debug("Get MtM + OtMNested {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get MtM + OtMNested {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.parentsJoinOtMFetch();
-            log.debug("Get P + OtM Fetch {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get P + OtM Fetch {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.childOtMJoinOtO();
-            log.debug("Get OtM + OtO {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get OtM + OtO {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             log.debug("Iterate");
             for (Parent p : dao.parentsFindAll()) {
                 log.trace(p.toString());
             }
-            count = dao.getStatistics().getPrepareStatementCount() - count;
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count;
         } finally {
             NPOApp.log.debug("Total queries {}", count);
             dao.getEntityManager().close();
@@ -87,20 +115,20 @@ public class NPOApp {
         Dao dao = daoProvider.get();
         long count = 0;
         try {
-            count = dao.getStatistics().getPrepareStatementCount();
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount();
             dao.parentsJoinMtM();
-            log.debug("Get P + MtM {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get P + MtM {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.childMtMJoinOtMNested();
-            log.debug("Get MtM + OtMNested {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get MtM + OtMNested {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.parentsJoinOtM();
-            log.debug("Get P + OtM {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get P + OtM {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.childOtMJoinOtO();
-            log.debug("Get OtM + OtO {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get OtM + OtO {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             log.debug("Iterate");
             for (Parent p : dao.parentsFindAll()) {
                 log.trace(p.toString());
             }
-            count = dao.getStatistics().getPrepareStatementCount() - count;
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count;
         } finally {
             NPOApp.log.debug("Total queries {}", count);
             dao.getEntityManager().close();
@@ -112,18 +140,18 @@ public class NPOApp {
         Dao dao = daoProvider.get();
         long count = 0;
         try {
-            count = dao.getStatistics().getPrepareStatementCount();
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount();
             dao.parentsJoinMtM();
-            log.debug("Get P + MtM {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get P + MtM {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.childMtMJoinOtMNested();
-            log.debug("Get MtM + OtMNested {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get MtM + OtMNested {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             dao.parentsOtMJoinOtMtO();
-            log.debug("Get P + OtM + OtO {}", dao.getStatistics().getPrepareStatementCount() - count);
+            log.debug("Get P + OtM + OtO {}", getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count);
             log.debug("Iterate");
             for (Parent p : dao.parentsFindAll()) {
                 log.trace(p.toString());
             }
-            count = dao.getStatistics().getPrepareStatementCount() - count;
+            count = getStatistics(dao.getEntityManager()).getPrepareStatementCount() - count;
         } finally {
             NPOApp.log.debug("Total queries {}", count);
             dao.getEntityManager().close();
@@ -133,10 +161,10 @@ public class NPOApp {
     private void init() {
         log.debug("*** Init test data");
         EntityManager em = entityManagerProvider.get();
+        long count = getStatistics(em).getPrepareStatementCount();
         EntityTransaction tx = em.getTransaction();
         try {
             tx.begin();
-            Random random = new Random();
             ChildMtM[] childMtMs = new ChildMtM[100];
             for (int i = 0; i < childMtMs.length; i++) {
                 childMtMs[i] = new ChildMtM();
@@ -148,7 +176,7 @@ public class NPOApp {
             for (int i = 0; i < childOtMNesteds.length; i++) {
                 childOtMNesteds[i] = new ChildOtMNested();
                 childOtMNesteds[i].setValue(UUID.randomUUID().toString());
-                childOtMNesteds[i].setParent(childMtMs[random.nextInt(childMtMs.length)]);
+                childOtMNesteds[i].setParent(childMtMs[i % childMtMs.length]);
 
                 em.persist(childOtMNesteds[i]);
             }
@@ -157,7 +185,7 @@ public class NPOApp {
             for (int i = 0; i < parents.length; i++) {
                 parents[i] = new Parent();
                 for (int j = 0; j < 10; j++) {
-                    parents[i].getChildMtM().add(childMtMs[random.nextInt(childMtMs.length)]);
+                    parents[i].getChildMtM().add(childMtMs[i % childMtMs.length]);
                 }
                 em.persist(parents[i]);
             }
@@ -166,7 +194,7 @@ public class NPOApp {
             for (int i = 0; i < childOtMs.length; i++) {
                 childOtMs[i] = new ChildOtM();
                 childOtMs[i].setValue(UUID.randomUUID().toString());
-                childOtMs[i].setParent(parents[random.nextInt(parents.length)]);
+                childOtMs[i].setParent(parents[i % parents.length]);
 
                 ChildOtO childOtO = new ChildOtO();
                 childOtO.setValue(UUID.randomUUID().toString());
@@ -182,7 +210,12 @@ public class NPOApp {
             log.error("Cannot init database", e);
             tx.rollback();
         } finally {
+            NPOApp.log.debug("Count {}", getStatistics(em).getPrepareStatementCount() - count);
             em.close();
         }
+    }
+
+    public Statistics getStatistics(EntityManager entityManager) {
+        return ((Session) entityManager.getDelegate()).getSessionFactory().getStatistics();
     }
 }
