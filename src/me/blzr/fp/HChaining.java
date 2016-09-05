bc4afa15b08dd907b3e4e9a58254407eb8b8513f
Constantine Linnick <theaspect@gmail.com>
2016-09-05 10:18:26 +0300

    Использование Optional

--- src/me/blzr/fp/HChaining.java
+++ src/me/blzr/fp/HChaining.java
@@ -1,32 +1,42 @@
 package me.blzr.fp;
 
+import java.util.Optional;
+
 import static me.blzr.fp.ZCommon.print;
 
 public class HChaining {
     public static void main(String[] args) {
+        // Иерархия классов
         A a1 = new A(new B(new C(10)));
 
+        // Простой случай
         print(a1.getB().getC().getVal());
 
-        //<editor-fold desc="safe">
-        //        A a2 = new A(null);
-//        if (a2.getB() != null &&
-//                a2.getB().getC() != null &&
-//                a2.getB().getC().getVal() != null) {
-//            print(a2.getB().getC().getVal());
-//        } else {
-//            print(0);
-//        }
-        //</editor-fold>
+        // Что если цепочка на каком-то элементе оборвется,
+        // Необходимо добавлять много проверок чтобы избежать NullPointerException
+        A a2 = new A(null);
+        if (a2.getB() != null &&
+                a2.getB().getC() != null &&
+                a2.getB().getC().getVal() != null) {
+            print(a2.getB().getC().getVal());
+        } else {
+            print(0);
+        }
 
-        //<editor-fold desc="orElse">
-        /*
+        // Функциональный подход, если встречаем null, заменяем его каким-то значением
         print(a2
-        .getBOpt().orElse(new B(null))
-        .getCOpt().orElse(new C(null))
-        .getValOpt().orElse(0));
-        */
-        //</editor-fold>
+                .getBOpt().orElse(new B(null))
+                .getCOpt().orElse(new C(null))
+                .getValOpt().orElse(0));
+
+        // Альтернативный подход
+        // В этом случае промежуточные вычисления проводится не будут.
+        // После первого null, вычисления будут сразу проброшены до последнего этапа
+
+        print(a2.getBOpt()
+                .flatMap(B::getCOpt)
+                .flatMap(C::getValOpt)
+                .orElse(0));
     }
 
     /**
@@ -47,9 +57,9 @@ public class HChaining {
             return b;
         }
 
-//        Optional<B> getBOpt() {
-//            return Optional.ofNullable(b);
-//        }
+        Optional<B> getBOpt() {
+            return Optional.ofNullable(b);
+        }
     }
 
     static class B {
@@ -63,9 +73,9 @@ public class HChaining {
             return c;
         }
 
-//        Optional<C> getCOpt() {
-//            return Optional.ofNullable(c);
-//        }
+        Optional<C> getCOpt() {
+            return Optional.ofNullable(c);
+        }
     }
 
     static class C {
@@ -79,8 +89,8 @@ public class HChaining {
             return val;
         }
 
-//        Optional<Integer> getValOpt() {
-//            return Optional.ofNullable(val);
-//        }
+        Optional<Integer> getValOpt() {
+            return Optional.ofNullable(val);
+        }
     }
 }
