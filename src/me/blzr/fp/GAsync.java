d61b5e737975d9c5d350baa2474edb8c2bb3144e
Constantine Linnick <theaspect@gmail.com>
2016-09-05 10:13:19 +0300

    Асинхронный код

--- src/me/blzr/fp/GAsync.java
+++ src/me/blzr/fp/GAsync.java
@@ -12,6 +12,12 @@ import static me.blzr.fp.ZCommon.sleep;
 
 public class GAsync {
     public static void main(String[] args) {
+        // Асинхронный код может поднять утилизацию вашей системы
+        // Треды вместо ожидания результата будут заняты другой работой
+
+        // Найдем пересечение двух списков, получим число из третьей системы и прибавим его к пересечению
+
+        // Императивный код ожидает выполнения каждого этапа
         List<Integer> a = requestDatabase1();
         List<Integer> b = requestDatabase2();
         List<Integer> joined = join(a, b);
@@ -24,6 +30,29 @@ public class GAsync {
         }
 
         print(result);
+
+        // Асинхронная версия. Каждый этап возвращает Future, которая будет содержать результать когда-нибудь в будущем
+        try {
+            CompletableFuture<Integer> fe = CompletableFuture.supplyAsync(GAsync::waitOnEvent);
+
+            CompletableFuture<List<Integer>> fa = CompletableFuture.supplyAsync(GAsync::requestDatabase1);
+            CompletableFuture<List<Integer>> fb = CompletableFuture.supplyAsync(GAsync::requestDatabase2);
+
+            // Мы комбинируем асинхронные Future
+            CompletableFuture<List<Integer>> fj = fa.thenCombineAsync(fb, GAsync::join);
+
+            List<Integer> asyncResult = fe.thenCombine(fj, (event, join) ->
+                    join.stream()
+                            .map(i -> i + event)
+                            .collect(Collectors.toList()))
+                    // До тех пор пока не понадобится получить результат
+                    .get();
+            print(asyncResult);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     static List<Integer> requestDatabase1() {
@@ -47,27 +76,4 @@ public class GAsync {
         sleep("waiting", 1000);
         return 10;
     }
-
-    //<editor-fold desc="supplyAsync">
-    public static void main2(String[] args) {
-        try {
-            CompletableFuture<Integer> fe = CompletableFuture.supplyAsync(GAsync::waitOnEvent);
-
-            CompletableFuture<List<Integer>> fa = CompletableFuture.supplyAsync(GAsync::requestDatabase1);
-            CompletableFuture<List<Integer>> fb = CompletableFuture.supplyAsync(GAsync::requestDatabase2);
-
-            CompletableFuture<List<Integer>> fj = fa.thenCombineAsync(fb, GAsync::join);
-
-            List<Integer> result = fe.thenCombine(fj, (event, join) ->
-                    join.stream()
-                            .map(i -> i + event)
-                            .collect(Collectors.toList())).get();
-            print(result);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
-    //</editor-fold>
 }
