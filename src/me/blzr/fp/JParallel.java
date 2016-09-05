05748d70d003e30313a0e2feb3fc0a7f1580565a
Constantine Linnick <theaspect@gmail.com>
2016-09-05 10:26:56 +0300

    Параллельная обработка данных

--- src/me/blzr/fp/JParallel.java
+++ src/me/blzr/fp/JParallel.java
@@ -9,6 +9,19 @@ import static me.blzr.fp.ZCommon.sleep;
 
 public class JParallel {
     public static void main(String[] args) {
+        // 5 сайтов, запрашиваем с каждого список из пяти чисел
+        // если результат получен оставляем только четные положительные числа
+
+        // Последовательная аггрегация
+        iterative();
+        // Конкурентная агрегация
+        concurrent();
+        // Функциональный подход
+        functional();
+    }
+
+    public static void iterative() {
+        print("Iterative");
         List<String> urls = Arrays.asList("site1.com", "site2.com", "site3.com", "site4.com", "site5.com");
 
         // Get all not null prices
@@ -33,24 +46,12 @@ public class JParallel {
         print(positiveEven);
     }
 
-    static List<Integer> getPrices(String url) {
-        sleep("Getting prices for " + url, 1000);
-
-        Random r = new Random();
-        if (r.nextFloat() > 0.8) {
-            return null;
-        }
-
-        List<Integer> prices = Arrays.asList(r.nextInt(), r.nextInt(), r.nextInt(), r.nextInt(), r.nextInt());
-        return prices;
-    }
-
-    //<editor-fold desc="Executor">
-    public static void main2(String[] args) throws ExecutionException, InterruptedException {
+    public static void concurrent() {
+        print("Concurrent");
         List<String> urls = Arrays.asList("site1.com", "site2.com", "site3.com", "site4.com", "site5.com");
 
+        // От предыдущего варианта отличается вот этим блоком
         ExecutorService es = Executors.newFixedThreadPool(4);
-
         List<Future<List<Integer>>> futures = new ArrayList<>();
         for (String u : urls) {
             futures.add(es.submit(new Callable<List<Integer>>() {
@@ -63,8 +64,14 @@ public class JParallel {
 
         List<Integer> results = new ArrayList<>();
         for (Future<List<Integer>> f : futures) {
-            if (f.get() != null) {
-                results.addAll(f.get());
+            try {
+                if (f.get() != null) {
+                    results.addAll(f.get());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
         }
 
@@ -80,14 +87,13 @@ public class JParallel {
         print(positiveEven);
         es.shutdown();
     }
-    //</editor-fold>
 
-    //<editor-fold desc="parallel">
-    public static void main3(String[] args) {
+    public static void functional() {
+        print("Functional");
         List<String> urls = Arrays.asList("site1.com", "site2.com", "site3.com", "site4.com", "site5.com");
 
         print(urls.stream()
-                .parallel()
+                .parallel() // Тут происходит магия
                 .map(JParallel::getPrices)
                 .filter(i -> i != null)
                 .flatMap(Collection::stream)
@@ -95,5 +101,17 @@ public class JParallel {
                 .filter(i -> i % 2 == 0)
                 .collect(Collectors.toList()));
     }
-    //</editor-fold>
+
+    // Генерация случайного списка или null с 20% вероятностью
+    static List<Integer> getPrices(String url) {
+        sleep("Getting prices for " + url, 1000);
+
+        Random r = new Random();
+        if (r.nextFloat() > 0.8) {
+            return null;
+        }
+
+        List<Integer> prices = Arrays.asList(r.nextInt(), r.nextInt(), r.nextInt(), r.nextInt(), r.nextInt());
+        return prices;
+    }
 }
