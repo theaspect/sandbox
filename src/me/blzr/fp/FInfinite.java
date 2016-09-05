eb525109761e7a1b4270dff0cd53a1bf8a5e74ff
Constantine Linnick <theaspect@gmail.com>
2016-09-05 10:09:01 +0300

    Бесконечные генераторы

--- src/me/blzr/fp/FInfinite.java
+++ src/me/blzr/fp/FInfinite.java
@@ -1,20 +1,18 @@
 package me.blzr.fp;
 
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import static me.blzr.fp.ZCommon.print;
 
 public class FInfinite {
     public static void main(String[] args) {
-        print("Ленивые вычисления");
-        // range peek
-
         print("Бесконечные генераторы");
 
-//        Infinite
-//        FifSupplier
-//        RandomSupplier
-
-    //        <editor-fold desc="Description">
-        /*
+        // Цепочный метод peek позволяет подглядеть на текущий элемент
         print(
         IntStream.range(0, 50)
                 .peek(System.out::println)
@@ -23,18 +21,18 @@ public class FInfinite {
                 .limit(10)
                 .sum());
 
+        // В бесконечный генератор, передается два аргумента: нулевой элемент, и лямбда для генерации следующего
         print(Stream.iterate("", s -> s + "+").limit(3).collect(Collectors.joining("")));
         print(Stream.iterate("", s -> s + "+").limit(5).collect(Collectors.joining(",")));
         print(Stream.iterate("!", s -> s + "+").limit(10).collect(Collectors.joining(",")));
-        print(Stream.generate(new FibSupplier()).skip(20).limit(10).collect(Collectors.toList()));
-        new Random().ints().filter(i -> i > 0 && i < 100).limit(10).mapToObj(Integer::toString).forEach(System.out::println);
-        */
-        //</editor-fold>
 
+        // Бесконечный генератор случайных чисел
+        new Random().ints().filter(i -> i > 0 && i < 100).limit(10).mapToObj(Integer::toString).forEach(System.out::println);
+
+        // Правильная реализация бесконечной последовательности Фиббоначи
+        print(Stream.generate(new FibSupplier()).skip(20).limit(10).collect(Collectors.toList()));
     }
 
-    //<editor-fold desc="Supplier">
-    /*
     static class FibSupplier implements Supplier<Integer> {
         int p0 = 0;
         int p1 = 1;
@@ -47,6 +45,4 @@ public class FInfinite {
             return res;
         }
     }
-    */
-    //</editor-fold desc="Description">
 }
