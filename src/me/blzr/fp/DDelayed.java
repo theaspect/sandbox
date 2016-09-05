bdbdf1be2c6e32edc505dedc5a11e1006c87ecaf
Constantine Linnick <theaspect@gmail.com>
2016-09-05 09:56:46 +0300

    Ленивые вычисления

--- src/me/blzr/fp/DDelayed.java
+++ src/me/blzr/fp/DDelayed.java
@@ -4,15 +4,24 @@ import java.util.Random;
 
 import static me.blzr.fp.ZCommon.print;
 import static me.blzr.fp.ZCommon.sleep;
+import static me.blzr.fp.ZCommon.Function;
 
 public class DDelayed {
 
     public static void main(String[] args) {
-        printIf(new Random().nextFloat() > 0.6, longCalc());
-    }
+        // Данный метод с 50% вероятностью вызывает длительные вычисления,
+        // но все аргументы функции printIf должны быть вычислены, на момент запуска
+        printIf(new Random().nextFloat() > 0.5, longCalc());
 
-    static void printIf(boolean cond, int value) {
-        if (cond) print(value);
+        // Чтобы не вызывать долгие вычисления мы бы написали примерно так
+        Float res = new Random().nextFloat();
+        if(res > 0.5){
+            print(longCalc());
+        }
+
+        // В функциональном подходе можно передать функцию, которую метод вызовет, если условие истинно
+        // DDelayed::longCalc то же самое что и () -> longCalc()
+        printIf2(new Random().nextFloat() > 0.5, DDelayed::longCalc);
     }
 
     static int longCalc() {
@@ -20,11 +29,11 @@ public class DDelayed {
         return 42;
     }
 
-    //<editor-fold desc="Example">
-    /*
-    static void printIfTrue2(boolean cond, Function<Integer> method) {
+    static void printIf(boolean cond, int value) {
+        if (cond) print(value);
+    }
+
+    static void printIf2(boolean cond, Function<Integer> method) {
         if (cond) print(method.apply());
     }
-    */
-    //</editor-fold>
 }
