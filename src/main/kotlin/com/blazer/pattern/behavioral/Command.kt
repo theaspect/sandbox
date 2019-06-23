20add2553c732c19145a1f8d987e220e070b4d45
Constantine Linnick <theaspect@gmail.com>
2019-06-24 02:12:32 +0700

    Add lambda version for Command

--- src/main/kotlin/com/blazer/pattern/behavioral/Command.kt
+++ src/main/kotlin/com/blazer/pattern/behavioral/Command.kt
@@ -11,8 +11,11 @@ object CommandDemo {
         invoker.register("on", onCommand)
         invoker.register("off", offCommand)
 
+        invoker.registerLambda("lambda") { lamp.lambda() }
+
         invoker.execute("on")
         invoker.execute("off")
+        invoker.execute("lambda")
     }
 }
 
@@ -20,22 +23,28 @@ class Invoker {
     private val commands: MutableMap<String, Command> = mutableMapOf()
 
     fun register(name: String, command: Command) = commands.put(name, command)
+    fun registerLambda(name: String, lambda: () -> Unit) = commands.put(
+            name,
+            object : Command() {
+                override fun execute() = lambda()
+            })
     fun execute(command: String) = commands[command]?.execute()
 }
 
-abstract class Command(val receiver: Lamp) {
+abstract class Command {
     abstract fun execute()
 }
 
-class OnLight(receiver: Lamp) : Command(receiver) {
+class OnLight(val receiver: Lamp) : Command() {
     override fun execute() = receiver.on()
 }
 
-class OffLight(receiver: Lamp) : Command(receiver) {
+class OffLight(val receiver: Lamp) : Command() {
     override fun execute() = receiver.off()
 }
 
 class Lamp {
     fun on() = println("Lamp turned on")
     fun off() = println("Lamp turned off")
+    fun lambda() = println("Lambda invocation")
 }
